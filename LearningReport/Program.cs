5535c91bf8cafbc0aea37d4f07215e27a3bfdbc6
absorkun <dwiyanaabsor@gmail.com>
2025-09-03 02:29:00 +0700

    Add Environment Variables Config

--- LearningReport/Program.cs
+++ LearningReport/Program.cs
@@ -10,6 +10,7 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddEnvironmentVariables();
 // Add services to the container.
 
 builder.Services.AddControllers();
